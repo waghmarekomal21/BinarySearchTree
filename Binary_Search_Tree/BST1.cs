8bafc1e7079989e492cecea15ab166583cc3596d
unknown <Komal>
2022-09-23 21:26:59 +0530

    [Komal] Create Binary Search Tree

---
+++ Binary_Search_Tree/BST1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    internal class BST1<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BST1<T> LeftTree { get; set; }
+        public BST1<T> RightTree { get; set; }
+        public BST1(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+        int leftCount = 0;
+        int rightCount = 0;
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new BST1<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BST1<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+
+    }
+}
